d05391a9d24314f660ce22a45b812b780a062773
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2018-09-21 00:31:52 +0530

    Init Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  url : string;
+
+  constructor(public sanitizer: DomSanitizer){}
+
+  ngOnInit() {
+    this.url = 'https://en.wikipedia.org/wiki/Hollywood';
+  }
+
+  switchTo(page){
+    if(page === "Hollywood"){
+      this.url = 'https://en.wikipedia.org/wiki/Hollywood';
+    }else if(page === "Bollywood"){
+      this.url = 'https://en.wikipedia.org/wiki/Bollywood';
+    }else{
+      this.url = 'https://en.wikipedia.org/wiki/Cinema_of_Odisha';
+    }
+  }
 }
